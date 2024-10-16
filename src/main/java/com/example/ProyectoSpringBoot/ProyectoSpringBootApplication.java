f2062773a70575512860f9a079ad186e49a76396
alvaroatue <atuealvaro@gmail.com>
2024-10-16 09:39:20 -0300

    Initial commit

---
+++ src/main/java/com/example/ProyectoSpringBoot/ProyectoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoSpringBoot/ProyectoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
